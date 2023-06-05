662213592a401499ee2e5c6d78c5c231d144511b
naingolan <kelvinsdechaw@gmail.com>
2023-06-05 14:22:57 +0300

    log me in master

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
 
 
 @Component({
@@ -8,12 +9,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent {
   userId: String ="";
-  constructor(private route: ActivatedRoute) { }
+  userName: String = "";
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+    ) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.userId = params['userId'];
       console.log('User ID:', this.userId);
+      this.userName = this.userService.getUserData().user.username;
       // Use the userId to fetch the user data or perform any other logic
     });
   }
